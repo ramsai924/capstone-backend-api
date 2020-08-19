3e25b990c567dd90bbbaea60e24f579fbf968e72
ramsai924 <ramsai924@gmail.com>
2020-08-19 20:23:57 +0530

    backend api v2 jwt Generated

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 //mongodb+srv://capstone:capstone@cluster0.azviz.mongodb.net/capstone-backend-api?retryWrites=true&w=majority
 //mongodb://localhost:27017/recycling
 const db = mongoose
-  .connect("mongodb://localhost:27017/recycling", {
+  .connect("mongodb+srv://capstone:capstone@cluster0.azviz.mongodb.net/capstone-backend-api?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
